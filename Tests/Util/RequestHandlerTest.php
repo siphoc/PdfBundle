7e5f03acd0fc4ef3b7902fc8a030bb6ca232835f
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2013-02-10 17:00:05 +0100

    RequestHandler: add basic Request handling.
    Trough Buzz, handle Request handling properly.

---
+++ Tests/Util/RequestHandlerTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Siphoc\PdfBundle\Tests\Util;
+
+use Siphoc\PdfBundle\Util\RequestHandler;
+
+class RequestHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_constructor_stores_mandatory_data()
+    {
+        $request = $this->getRequestMock();
+        $response = $this->getResponseMock();
+        $client = $this->getClientMock();
+
+        $handler = new RequestHandler($request, $response, $client);
+        $this->assertSame($request, $handler->getRequest());
+        $this->assertSame($response, $handler->getResponse());
+        $this->assertSame($client, $handler->getClient());
+    }
+
+    public function test_it_returns_url_content()
+    {
+        $handler = $this->getRequestHandler();
+        $content = $handler->getContent(
+            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js'
+        );
+
+        $this->assertEquals('get content', $content);
+    }
+
+    private function getRequestMock()
+    {
+        $request =  $this->getMock(
+            'Buzz\Message\Request'
+        );
+
+        return $request;
+    }
+
+    private function getResponseMock()
+    {
+        $response =  $this->getMock(
+            'Buzz\Message\Response'
+        );
+
+        $response->expects($this->any())
+            ->method('getContent')
+            ->will($this->returnValue('get content'));
+
+        return $response;
+    }
+
+    private function getClientMock()
+    {
+        $client =  $this->getMock(
+            '\Buzz\Client\FileGetContents',
+            array('send')
+        );
+
+        return $client;
+    }
+
+    private function getRequestHandler()
+    {
+        $handler = new RequestHandler(
+            $this->getRequestMock(),
+            $this->getResponseMock(),
+            $this->getClientMock()
+        );
+
+        return $handler;
+    }
+}
---
+++ Util/RequestHandler.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Siphoc
+ *
+ * @author      Jelmer Snoeck <jelmer@siphoc.com>
+ * @copyright   2013 Siphoc
+ * @link        http://siphoc.com
+ */
+
+namespace Siphoc\PdfBundle\Util;
+
+use Buzz\Client\ClientInterface;
+use Buzz\Message\MessageInterface;
+
+/**
+ * The handler that we'll use to get external files from other servers.
+ *
+ * @author Jelmer Snoeck <jelmer@siphoc.com>
+ */
+class RequestHandler
+{
+    /**
+     * The request object we'll be using to do HTTP calls.
+     *
+     * @var MessageInterface
+     */
+    protected $request;
+
+    /**
+     * The response object we'll be using to store our response data in.
+     *
+     * @var MessageInterface
+     */
+    protected $response;
+
+    /**
+     * The client object we'll be using to do our actuall calls.
+     *
+     * @var ClientInterface
+     */
+    protected $client;
+
+    /**
+     * Initiate the Request handler with a Request, Response and Client
+     * interface.
+     *
+     * @param MessageInterface $request
+     * @param MessageInterface $response
+     * @param ClientInterface $client
+     */
+    public function __construct(MessageInterface $request,
+        MessageInterface $response, ClientInterface $client)
+    {
+        $this->request = $request;
+        $this->response = $response;
+        $this->client = $client;
+    }
+
+    /**
+     * Fetch the Client object we're using to do requests.
+     *
+     * @return MessageInterface
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * Retrieve the contents from a given url.
+     *
+     * @param string $url
+     * @return string
+     */
+    public function getContent($url)
+    {
+        $this->getRequest()->setHost($url);
+        $this->getClient()->send(
+            $this->getRequest(),
+            $this->getResponse()
+        );
+
+        return $this->getResponse()->getContent();
+    }
+
+    /**
+     * Fetch the Response object we're using to do requests.
+     *
+     * @return MessageInterface
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+    /**
+     * Fetch the Request object we're using to do requests.
+     *
+     * @return MessageInterface
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
+}
